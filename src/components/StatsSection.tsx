9e90753f93603c766c8881983407ba4603522ce0
Oluwaseyi David <oluwaseyidavid26@gmail.com>
2025-10-30 17:42:36 -0700

    commit4

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import  { useState, useEffect, useRef } from "react";
 import { Home, Users, FileText } from "lucide-react";
 
 interface StatItem {
